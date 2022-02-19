7f75b2cb028c8ebdbf4365022c1f1c9f5b6a33a3
YxYuuu <kris_xiaoyu@163.com>
2022-02-19 12:03:37 +0800

    first commit

---
+++ learning/test.py
@@ -0,0 +1,46 @@
+'''
+测试
+if True:
+   print(2)
+else:
+    print(1) 
+'''
+
+
+'''
+str='123456789'
+
+print(str)   #输出字符串
+print(str[0:-1])  #输出第一个字符串到倒数第二个字符串
+print(str[0])  #输出第1个字符串
+print(str[2:5]) #输出第3个到第5个字符串
+print(str[2:]) #输出第3个字符串开始后所有字符串
+print(str[1:10:2]) #输出从第2个开始到第5个，且每隔1个字符（步长为2）
+print(str * 2)
+print(str + '你好')
+
+print('----' * 4)
+
+print('hello\nrunoob')
+print(r'hello\nrunoob')
+
+'''
+
+'''
+input_test=input("\n\n请输入一个数字，按下enter键后退出\n")
+print(input_test)
+
+'''
+
+# import sys;x='runoob';sys.stdout.write(x+'\n')
+
+
+
+
+counter = 100   # 整形变量
+miles =100.00   # 浮点型变量
+name = "runoob" # 字符串
+
+print(counter,  end=' ')
+print(miles)
+print(name)
